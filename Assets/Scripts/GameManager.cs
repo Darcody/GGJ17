4fd451d1cba641ad4f6bf62f803abb66919a085e
Colin Drude <colin.drude@googlemail.com>
2017-01-20 23:49:42 +0100

    - added first Networking attempts - added GameJamMenu Template

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Determins if Lighthouse (0) or Boat (1)
+    public static int playerPosition = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
