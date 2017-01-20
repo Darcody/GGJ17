9029f67166be47f6d91ce15e1cbcbb8683490431
Fabian Müller <fabianmueller5@gmx.net>
2017-01-20 23:27:09 +0100

    - boat controls test

---
+++ Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    [SerializeField] float maxTurningPerFrame = 10.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float dist;
+            Plane plane = new Plane(Vector3.up, transform.position);
+            if (plane.Raycast(ray, out dist))
+            {
+                Quaternion rotation = Quaternion.LookRotation(ray.GetPoint(dist) - transform.position);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, maxTurningPerFrame * Time.deltaTime);
+            }
+        }
+
+    }
+}
