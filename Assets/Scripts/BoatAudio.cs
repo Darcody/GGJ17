648436bb66c51b21d4162e8b328d37f0c1c22e21
Fabian Müller <fabianmueller5@gmx.net>
2017-01-22 00:16:20 +0100

    - Audio

---
+++ Assets/Scripts/BoatAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatAudio : MonoBehaviour
+{
+    [SerializeField] AudioClip[] allClips;
+    AudioSource mySource;
+
+    void Start()
+    {
+        mySource = GetComponent<AudioSource>();
+        mySource.clip = allClips[0];
+        mySource.loop = true;
+        mySource.Play();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
