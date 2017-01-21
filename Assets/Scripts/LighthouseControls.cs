d87dc4cd84c5b7a18cf3d9e2d24c140076585e55
Fabian Müller <fabianmueller5@gmx.net>
2017-01-21 10:58:36 +0100

    - Light Movement and light controls are separated

--- Assets/Scripts/LighthouseControls.cs
+++ Assets/Scripts/LighthouseControls.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LighthouseControls : MonoBehaviour
 {
-    [SerializeField] private float m_rotationSpeed = 5.0f;
     private GameObject m_lightTarget;
     private GameObject m_light;
 
@@ -24,9 +23,6 @@ public class LighthouseControls : MonoBehaviour
             if (Physics.Raycast(mouseRay, out hit ))
             {
                 m_lightTarget.transform.position = hit.point;
-                Vector3 direction = (m_lightTarget.transform.position - m_light.transform.transform.position).normalized;
-                Quaternion lookRotation = Quaternion.LookRotation(direction);
-                m_light.transform.rotation = Quaternion.Slerp(m_light.transform.transform.rotation, lookRotation, Time.deltaTime * m_rotationSpeed);
             }
         }
     }
---
+++ Assets/Scripts/LighthouseLightMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseLightMovement : MonoBehaviour {
+    [SerializeField] private float m_rotationSpeed = 5.0f;
+    private GameObject m_lightTarget;
+    private GameObject m_light;
+
+    void Start()
+    {
+        m_lightTarget = transform.FindChild("LightHouseTarget").gameObject;
+        m_light = transform.FindChild("Spotlight").gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 direction = (m_lightTarget.transform.position - m_light.transform.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        m_light.transform.rotation = Quaternion.Slerp(m_light.transform.transform.rotation, lookRotation, Time.deltaTime * m_rotationSpeed);
+    }
+}
