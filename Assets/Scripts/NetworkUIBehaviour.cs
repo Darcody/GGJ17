5f9f64c8d21da3c8829e14c0c4f9bf8028a1d72c
Colin Drude <colin.drude@googlemail.com>
2017-01-21 21:56:32 +0100

    - bug fix?

--- Assets/Scripts/NetworkUIBehaviour.cs
+++ Assets/Scripts/NetworkUIBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -11,19 +12,57 @@ public class NetworkUIBehaviour : MonoBehaviour {
 
     public void onHostGameClicked()
     {
-        networkController.GetComponent<NetworkController>().StartHost();
-        this.gameObject.SetActive(false);
+        NetworkController manager = networkController.GetComponent<NetworkController>();
+        if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
+        {
+            manager.StartHost();
+            this.gameObject.SetActive(false);
+        }
     }
 
     public void onJoinGameClicked()
     {
-        networkController.GetComponent<NetworkController>().StartClient();
-        networkController.GetComponent<NetworkController>().networkAddress = addressField.text;
-        this.gameObject.SetActive(false);
+
+        NetworkController manager = networkController.GetComponent<NetworkController>();
+        if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
+        {
+            manager.networkAddress = addressField.text;
+            manager.StartClient();
+            this.gameObject.SetActive(false);
+        }
     }
+
+    public void Update()
+    {
+        if (NetworkServer.active)
+        {
+          Debug.Log("Server: port=" + networkController.GetComponent<NetworkController>().networkPort);
+        }
+        if (NetworkClient.active)
+        {
+            Debug.Log( "Client: address=" + networkController.GetComponent<NetworkController>().networkAddress + " port=" + networkController.GetComponent<NetworkController>().networkPort);
+        }
+
+        if (NetworkClient.active && !ClientScene.ready)
+        {
+            
+                ClientScene.Ready(networkController.GetComponent<NetworkController>().client.connection);
+
+                if (ClientScene.localPlayers.Count == 0)
+                {
+                    ClientScene.AddPlayer(0);
+                }
+            }
+        }
+    
 
     public void onBackClicked()
     {
         SceneManager.LoadScene("StartScene");
+    }
+
+    public void OnApplicationQuit()
+    {
+        networkController.GetComponent<NetworkController>().StopHost();
     }
 }
