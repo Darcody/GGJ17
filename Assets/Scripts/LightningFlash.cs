1403f302778546d29d845d0ef2e90a0c9fd63537
darcody <d-jordan@gmx.net>
2017-01-22 14:42:31 +0100

    -audio

--- Assets/Scripts/LightningFlash.cs
+++ Assets/Scripts/LightningFlash.cs
@@ -7,15 +7,24 @@ public class LightningFlash : MonoBehaviour {
     float timer = 5f;
     float flashDuration = 1f;
     public Light flashSource;
-	
-	// Update is called once per frame
-	void Update () {
+    public AudioClip[] list;
+    AudioSource audio;
+    private void Start()
+    {
+        
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         if(timer <= 0f)
         {
             flashDuration = Random.value;
             timer = Random.value * 2.5f;
             //flashSource.intensity = Random.value;
             flashSource.enabled = true;
+            audio.clip = list[Random.Range(0, 5)];
+            audio.Play();
         }
         if(flashSource.enabled && flashDuration <= 0f)
         {
